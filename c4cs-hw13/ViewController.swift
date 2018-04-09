fac04a57ba27d576000d7915b873869951beb717
Nicole Duran <ndurann@umich.edu>
2018-04-09 15:46:35 -0400

    first commit

---
+++ c4cs-hw13/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  c4cs-hw13
+//
+//  Created by Pinto Bean on 4/9/18.
+//  Copyright © 2018 Nikki Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
